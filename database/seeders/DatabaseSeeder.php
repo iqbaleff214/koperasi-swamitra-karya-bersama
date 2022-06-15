7c500d3449f6fecc1f4b90e23b2a2c4ae012ad42
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-06-15 21:37:55 +0800

    add users seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,35 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $users = [
+            [
+                'name' => 'Manajer',
+                'email' => 'manajer@swamitra.com',
+                'password' => Hash::make('admin'),
+                'role' => 'manager',
+                'phone' => '0821',
+                'joined_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Teller',
+                'email' => 'teller@swamitra.com',
+                'password' => Hash::make('admin'),
+                'role' => 'teller',
+                'phone' => '0822',
+                'joined_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Collector',
+                'email' => 'kolektor@swamitra.com',
+                'password' => Hash::make('admin'),
+                'role' => 'collector',
+                'phone' => '0823',
+                'joined_at' => Carbon::now(),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
