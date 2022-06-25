84ea15bfd8cfe88c6c3c57413558f178413e6cbf
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-06-26 01:25:33 +0800

    change name to username

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -11,7 +11,7 @@
                                 <img src="{{ asset('swamitra.jpeg') }}" width="100%" class="p-4" alt="{{ config('app.name') }}" srcset="">
                             </div>
                             <div class="col-10">
-                                <h2 class="my-3">Hai, {{ Auth::user()->name }}</h2>
+                                <h2 class="my-3">Hai, {{ Auth::user()->username }}</h2>
                                 <p class="card-text h4 font-weight-light">
                                     Selamat datang di halaman dashboard {{ config('app.name') }} Banjarmasin.
                                 </p>
