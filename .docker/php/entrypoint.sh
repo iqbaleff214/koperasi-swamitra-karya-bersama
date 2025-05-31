c4d83594a1e265ee458a9d0a375961409bbf7361
M. Iqbal Effendi <iqbaleff214@gmail.com>
2025-05-31 08:49:21 +0800

    docker

---
+++ .docker/php/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+until mysqladmin ping -h db --silent; do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
+if [ ! -f /var/www/.migrated ]; then
+  php artisan migrate:fresh --force --seed
+  touch /var/www/.migrated
+fi
+
+php artisan optimize
+php artisan config:cache
+
+exec "$@"
